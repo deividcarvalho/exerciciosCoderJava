cddb232d25ca8561f96f8aa8ab3725b115e7130e
Deivid Carvalho <deivid.carvalho.cruz@gmail.com>
2022-06-06 22:28:28 -0300

    ExerciciosCoderJava

--- src/classe/PrimeiroTrauma.java
+++ src/classe/PrimeiroTrauma.java
@@ -5,7 +5,9 @@ public class PrimeiroTrauma {
 	
 	public static void main(String[] args) {
 					
-	System.out.println("Iniciar exercícios e desafios do curso 2022");
+	System.out.println("Iniciar exercÃ­cios e desafios do curso 2022");
+
+	System.out.println("Esse ano promete!!!");
 	
 	}
 
