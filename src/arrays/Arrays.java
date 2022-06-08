373b865317a6071b2d12f0dc51eb69a51756fc57
Deivid Carvalho <deivid.carvalho.cruz@gmail.com>
2022-06-07 21:21:09 -0300

    AprendendoArrays

---
+++ src/arrays/Arrays.java
@@ -0,0 +1,18 @@
+package arrays;
+
+public class Arrays {
+	
+	public static void main(String[] args) {
+		String[] a = new String[10];
+		a[0] = "Deivid";
+		a[3] = "Vanêssa";
+		a[1] = "Nanda";
+		a[2] = "Lipe";
+		
+		System.out.println(a[0].toUpperCase());
+		System.out.println(a[1].toLowerCase());
+		System.out.println(a[2].toString());
+		System.out.println(a[3]);
+	}
+
+}
--- src/classe/desafio/Jantar.java
+++ src/classe/desafio/Jantar.java
@@ -24,16 +24,7 @@ public class Jantar {
 	p2.comer(c3);
 	System.out.println(p2.apresentar());
 	
-	String[] a = new String[10];
-	a[0] = "Deivid";
-	a[3] = "Vanêssa";
-	a[1] = "Nanda";
-	a[2] = "Lipe";
-	
-	System.out.println(a[0].toUpperCase());
-	System.out.println(a[1].toLowerCase());
-	System.out.println(a[2].toString());
-	System.out.println(a[3]);
+
 	
 	
 	
