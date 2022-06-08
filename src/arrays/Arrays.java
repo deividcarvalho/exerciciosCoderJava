ab4466f496423e0904f88bdac4675d2bd85b3561
Deivid Carvalho <deivid.carvalho.cruz@gmail.com>
2022-06-07 21:28:06 -0300

    Organizando o código

--- src/arrays/Arrays.java
+++ src/arrays/Arrays.java
@@ -3,16 +3,24 @@ package arrays;
 public class Arrays {
 	
 	public static void main(String[] args) {
+		
+		//Array é um objeto
+		//É homogêneo (tem um mesmo tipo)
+		//É estático (Tamanho Fixo definido na criação)
+		//O tamanho não pode ser alterado, mas pode ser atribuido a um outro Array
+		//Pode ser multidimensional
+		
+		
 		String[] a = new String[10];
 		a[0] = "Deivid";
-		a[3] = "Vanêssa";
 		a[1] = "Nanda";
 		a[2] = "Lipe";
+		a[3] = "Vanêssa";
 		
 		System.out.println(a[0].toUpperCase());
 		System.out.println(a[1].toLowerCase());
 		System.out.println(a[2].toString());
-		System.out.println(a[3]);
+		System.out.println(a[3].length());
 	}
 
 }
